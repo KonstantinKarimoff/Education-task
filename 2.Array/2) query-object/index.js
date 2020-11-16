ba9b987fd16799ea7ac013318705ac49bd3e2d48
Konstantin Karimoff <73893991+KonstantinKarimoff@users.noreply.github.com>
2020-11-16 13:37:17 +0300

    Update index.js

--- 2.Array/2) query-object/index.js
+++ 2.Array/2) query-object/index.js
@@ -1,13 +1,10 @@
   function queryToObject(query) {
-
   if (query === "" || query === "?" ){
       return {};
   }
-
   const str = "{ " + query.replace(/\?/i, " \"").replace(/\=/g, "\" \: \"").replace(/\&/g, "\", \"") + "\"  " + " }";
   console.log(str);
   const obj = JSON.parse(str);
-
   for (let key in obj) {
       let isNum = !(isNaN(obj[key]));
       if (isNum) {
@@ -26,9 +23,6 @@
       }
   }
   return obj;
-
-
-
 };
 
 window.queryToObject = queryToObject;
