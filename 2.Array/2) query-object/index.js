e4e3038e31a3a9b3b7a7825a01d9e1806dd23c6c
Konstantin Karimoff <73893991+KonstantinKarimoff@users.noreply.github.com>
2020-11-16 13:36:46 +0300

    Update index.js

--- 2.Array/2) query-object/index.js
+++ 2.Array/2) query-object/index.js
@@ -1,12 +1,35 @@
-     function queryToObject(query) {
-        let f = query.split('?').join('');
-        return f.split('&').map((value) => value.split('=', 2))
-      .reduce((obj, value) => {
-        obj[value[0]] = value[1];
-        return obj;
-      }, {});
-    }
+  function queryToObject(query) {
 
+  if (query === "" || query === "?" ){
+      return {};
+  }
+
+  const str = "{ " + query.replace(/\?/i, " \"").replace(/\=/g, "\" \: \"").replace(/\&/g, "\", \"") + "\"  " + " }";
+  console.log(str);
+  const obj = JSON.parse(str);
+
+  for (let key in obj) {
+      let isNum = !(isNaN(obj[key]));
+      if (isNum) {
+          obj[key] = parseInt(obj[key]);
+      } else {
+          switch (obj[key]) {
+              case "true":
+                  obj[key] = true;
+                  break;
+              case "false":
+                  obj[key] = false;
+                  break;
+              default:
+                  break;
+          }
+      }
+  }
+  return obj;
+
+
+
+};
 
 window.queryToObject = queryToObject;
 
