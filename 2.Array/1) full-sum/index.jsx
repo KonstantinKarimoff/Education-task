88b958bf417c20fd298b00d74e53dbb1d25107a4
Konstantin Karimoff <73893991+KonstantinKarimoff@users.noreply.github.com>
2020-11-16 13:02:22 +0300

    Update index.jsx

--- 2.Array/1) full-sum/index.jsx
+++ 2.Array/1) full-sum/index.jsx
@@ -1,12 +1,4 @@
-import React from 'react';
-/**
- * @name Full-sum
- * @task
- * Написать функцию суммирования, которая будет суммировать все аргументы и возвращять результат
- * Если хотя бы один из арументов не является числом, сгенерировать ошибку c сообщением: "Wrong Argument Type"
- * Если аргументов не будет, пусть вернется 0
- * @param args
- */
+
 function fullSum(...args) {
     for (let i = 0; i < args.length; i++) {
         if (typeof args[i] !== 'number') throw new Error("Wrong Argument Type");
@@ -26,17 +18,3 @@ function fullSum(...args) {
 window.fullSum = fullSum;
 
 
-class Component extends React.component {
-  constructor() {
-    super();
-
-    this.state = {
-      config: false,
-    };
-  }
-
-  render() {
-    return <div>ddd</div>
-  }
-}
-export default Component;
